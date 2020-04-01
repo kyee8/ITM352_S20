6f6115779cfb3ae05ba207f69c620d88fa353022
kyee8 <kyee8@hawaii.edu>
2020-03-31 20:45:54 -1000

    new stuff

--- Lab 11/ex4.js
+++ Lab 11/ex4.js
@@ -16,7 +16,7 @@ errors = []; // assume no errors at first
 if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
 if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
 if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-returnErrors ? errors : (errors.length == 0);
+return returnErrors ? errors : (errors.length == 0);
 }
 
 pieces.forEach(function (item, index) {
